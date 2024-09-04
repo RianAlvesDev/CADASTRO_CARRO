de31af3acd23ba5525b720b41be99a40b7cf7a9a
Rian Alves <alvesrian49.com@gmail.com>
2024-09-04 20:58:40 -0300

    Atualização

--- Cadastro/src/components/Menu.vue
+++ Cadastro/src/components/Menu.vue
@@ -1,7 +1,5 @@
 <template>
     <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/login">Login</RouterLink>
       </nav>
 </template>
--- Cadastro/src/views/LoginView.vue
+++ Cadastro/src/views/LoginView.vue
@@ -1,9 +1,6 @@
 <script setup>
 
-    import {ref, reactive, onMounted} from 'vue';         //REF: alterações em tempo real.
- 
-    const email = ref('');
-    const password = ref('');
+    import {reactive, onMounted} from 'vue';         //REF: alterações em tempo real.
 
     const user = reactive({
         email:'',
@@ -22,17 +19,30 @@
 
 <template>
 
-   {{ user.email }}
-
-   <template v-if="user.email.length < 0">Email vazio</template>
-
-    <form action="" v-on:submit.prevent="login" >   
-        <input type="text" placeholder="Email" v-model="user.email">
-        <input type="text" placeholder="Password" v-model="user.password">
+    <form action="" v-on:submit.prevent="login" > 
+        <span class="painel" id="painel-text">Login</span>
+          
+        <div id="email" class="email"><input type="text" placeholder="Email" v-model="user.email"></div>
+        <input type="text" placeholder="Senha" v-model="user.password">
         <button type="submit">Login</button>
     </form>
 
 </template>
 
 <style>
+
+    * {
+       background-color: rgb(13, 28, 41);
+    }
+
+    button {
+        color: rgb(0, 0, 0);
+        background-color: rgb(255, 255, 255);
+    };
+    .email {
+         margin-bottom: 90px;
+    };
+    .painel-text {
+        color: aqua;
+    }
 </style>
